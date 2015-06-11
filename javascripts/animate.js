01b9cd296fd958cf662ac8f7e89e2876fd13fa9e
goooooooogle <kangxilord@gmail.com>
2015-06-11 19:03:42 +0800

    adjust mouse event

--- javascripts/animate.js
+++ javascripts/animate.js
@@ -23,11 +23,15 @@ $(function() {
     var percent;
 
     percent = (e.pageX / maxWidth) * 100;
-    movemove('.color-lines .layer-1', (percent - 50) * -.3);
-    movemove('.color-lines .layer-2', (percent - 50) * .5);
-    movemove('.color-lines .layer-3', (percent - 50) * -.7);
-    movemove('.color-lines .layer-4', (percent - 50) * .7);
-    movemove('.color-dots', (percent - 50) * -.9);
+    movemove('.color-lines .layer-1', (percent - 50) * -1.3);
+    movemove('.color-lines .layer-2', (percent - 50) * .4);
+    movemove('.color-lines .layer-3', (percent - 50) * -.5);
+    movemove('.color-lines .layer-4', (percent - 50) * .6);
+    movemove('.color-lines .layer-5', (percent - 50) * .7);
+    movemove('.color-lines .layer-6', (percent - 50) * -.9);
+    movemove('.color-lines .layer-7', (percent - 50) * -1.5);
+    movemove('.color-lines .layer-8', (percent - 50) * -1);
+    movemove('.color-dots', (percent - 50) * .3);
     movemove('.intro-right-lines', (percent - 50) * .3);
     return movemove('.intro-left-lines', (percent - 50) * -.3);
   });
