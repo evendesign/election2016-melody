a05b8c9dd917f5a340ae0b9c84743683fff86279
goooooooogle <kangxilord@gmail.com>
2015-06-23 10:35:47 +0800

    update

--- javascripts/song.js
+++ javascripts/song.js
@@ -29,7 +29,6 @@ $(function() {
     return $.getJSON('http://api.iing.tw/soundclouds/' + id + '.json?token=8888', function(item) {
       var songWaveform, waveform;
 
-      xx(item);
       $('.song-title').text(item.title);
       $('.song-artist').text(item.author_name);
       $('.song-number').text(padLeft(item.id, 3));
